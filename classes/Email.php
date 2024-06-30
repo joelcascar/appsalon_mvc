55fca35064d5b72043ef863c35c964c692a651b3
Joel Castellanos <joel.adcascar@gmail.com>
2024-06-29 19:53:43 -0600

    Actualizacion de email

--- classes/Email.php
+++ classes/Email.php
@@ -37,7 +37,7 @@ class Email{
             $email->Password = $_ENV["EMAIL_PASS"];
         	$email->SMTPSecure = "tls";
             // Configurar el contenido del email
-            $email->setFrom("appsalon.mvc.c@gmail.com");
+            $email->setFrom("joel.adcascar@gmail.com", "appsalon.com");
             $email->addAddress($this->email);
             $email->Subject = "Confirma tu cuenta";
             // Habilitar HTML
@@ -45,9 +45,9 @@ class Email{
             $email->CharSet = "UTF-8";
             // Definimos el contenido
             $contenido = "<html>";
-            $contenido .= "<p>Hola <strong> {$this->nombre} </strong> Has creado tu cuenta en App Salon, solo debes confirmarla presionando el siguiente enlace</p>";
-            $contenido .= "<p>Presiona aquí: <a href=\"{$_ENV["APP_URL"]}/confirmar-cuenta?token={$this->token}\">Confirmar Cuenta</a></p>";
-            $contenido .= "<p>Si tu no solicitaste esta cuenta, puedes ignorar el mensaje</p>";
+            $contenido .= "<p> Hola <strong> {$this->nombre} </strong> Has creado tu cuenta en App Salon, solo debes confirmarla presionando el siguiente enlace</p>";
+            $contenido .= "<p> Presiona aquí: <a href=\"{$_ENV["APP_URL"]}/confirmar-cuenta?token={$this->token}\">Confirmar Cuenta</a></p>";
+            $contenido .= "<p> Si tu no solicitaste esta cuenta, puedes ignorar el mensaje</p>";
             $contenido .= "</html>";
             $email->Body = $contenido;
             // Enviar el email
@@ -79,7 +79,7 @@ class Email{
             $email->Password = $_ENV["EMAIL_PASS"];
         	$email->SMTPSecure = "tls";
             // Configurar el contenido del email
-            $email->setFrom("appsalon.mvc.c@gmail.com");
+            $email->setFrom("joel.adcascar@gmail.com", "appsalon.com");
             $email->addAddress($this->email);
             $email->Subject = "Reestablece tu contraseña";
             // Habilitar HTML
@@ -87,9 +87,9 @@ class Email{
             $email->CharSet = "UTF-8";
             // Definimos el contenido
             $contenido = "<html>";
-            $contenido .= "<p>Hola<strong>{$this->nombre}</strong> Has solicitado reestablecer tu contraseña, sigue el siguiente enlace para hacerlo</p>";
-            $contenido .= "<p>Presiona aqui: <a href=\"{$_ENV["APP_URL"]}/recuperar?token={$this->token}\">Reestablecer contraseña</a></p>";
-            $contenido .= "<p>si tu no solicitaste esto, puedes ignortar el mensaje</p>";
+            $contenido .= "<p> Hola <strong> {$this->nombre} </strong> Has solicitado reestablecer tu contraseña, sigue el siguiente enlace para hacerlo </p>";
+            $contenido .= "<p> Presiona aqui: <a href=\"{$_ENV["APP_URL"]}/recuperar?token={$this->token}\"> Reestablecer contraseña </a></p>";
+            $contenido .= "<p> Si tu no solicitaste esto, puedes ignortar el mensaje </p>";
             $contenido .= "</html>";
             $email->Body = $contenido;
             // Enviar email
--- views/auth/mensaje.php
+++ views/auth/mensaje.php
@@ -1,2 +1,5 @@
 <h1 class="nombre-pagina">Confirma tu cuenta</h1>
 <p class="descripcion-pagina">Hemos enviado las instrucciones para confirmar a tu email</p>
+<div class="acciones">
+    <a href="/">Regresar al inicio</a>
+</div>
